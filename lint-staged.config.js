3161b64b21452b1183c72567631afb7b0e45ada1
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2025-07-02 10:24:09 -0400

    fix: ignore not supported languages

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
-	'**/*.{html,js,jsx,svelte,ts,tsx,vue,md,mdx,json}': filenames => [
+	'**/*.{html,js,jsx,ts,tsx,json}': filenames => [
 		`npx biome check --write ${filenames.map(filename => `"${filename}"`).join(' ')}`
 	],
 	'**/*.astro': filenames =>
