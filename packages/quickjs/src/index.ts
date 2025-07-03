fd68ed2332e06aa905776e5f788780b35a6707fc
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2025-07-03 19:35:10 -0400

    fix: quickjs related file not in bundle
    vite doesn't resolve quickjs files when loading with ?url, so I make a custom loadQuickJs function
    that uses quickjs directly in order to vite bundle it properly

--- packages/quickjs/src/index.ts
+++ packages/quickjs/src/index.ts
@@ -1,1 +1,28 @@
-export * from '@sebastianwessel/quickjs'
+import quickJsNgWasmVariant from '@jitl/quickjs-ng-wasmfile-release-sync'
+import {
+	loadQuickJs as _loadQuickJsFromSebastianWessel,
+	type ErrorResponse,
+	type OkResponse,
+	type SandboxFunction,
+	type SandboxOptions
+} from '@sebastianwessel/quickjs'
+import { newQuickJSWASMModuleFromVariant } from 'quickjs-emscripten-core'
+
+async function loadQuickJs(): Promise<ReturnType<typeof _loadQuickJsFromSebastianWessel>> {
+	try {
+		const wasmModuleInstance = await newQuickJSWASMModuleFromVariant(quickJsNgWasmVariant)
+		const quickJsInstance = await _loadQuickJsFromSebastianWessel(wasmModuleInstance)
+		return quickJsInstance
+	} catch (error) {
+		console.error('Error al cargar QuickJS en loadQuickJs():', error)
+		throw error
+	}
+}
+
+export {
+	loadQuickJs,
+	type SandboxOptions,
+	type ErrorResponse,
+	type OkResponse,
+	type SandboxFunction
+}
